23a669b046c0e8701de05b9ea74d14818772bb0c
Maksim-1307 <max.07mal@gmail.com>
2024-04-12 00:08:55 +0300

    borders resizable. border line added

--- $src/js/nice-border.js
+++ $src/js/nice-border.js
@@ -1,4 +1,23 @@
+$(window).on('resize', function(){
+    nice_border();
+});
 $(function(){
+    nice_border();
+});
+
+function rm_px(value, toFloat){
+    if (toFloat){
+        return parseFloat(rm_px(value));
+    } else {
+        return Math.round(rm_px(value));
+    }
+}
+
+function rm_px(value){
+    return value.slice(0, -2);
+}
+
+function nice_border(){
     $('.nice-border').each(function(){
 
         $(this).append("\
@@ -23,13 +42,13 @@ $(function(){
         element = $(this).find('.nice-border__element');
         width = parseFloat($(this).css("width").slice(0, -2)) + borderWidth * 2;
         height = parseFloat($(this).css("height").slice(0, -2)) + borderWidth * 2;
-        console.log(width, height)
         element.css({
             "top" : -borderWidth + "px",
             "left": -borderWidth + "px",
             "width":  width + "px",
             "height":  height + "px",
         });
+
         i = 0;
         $('.nice-border__edge').each(function(){
             rotate = 0;
@@ -42,5 +61,19 @@ $(function(){
             $(this).css("transform", "rotate(" + (90 * (i%4)) + "deg)")
             i++;
         });
+
+        const lineLen = 40.0; // width gap
+        $('.nice-border__horizontal').each(function(){
+            $(this).empty();
+            width = $(this).css('width');
+            linesCount = Math.round(rm_px(width, true) / lineLen);
+            $(this).append(('<div class="nice-border__h-line"></div>').repeat(linesCount));
+        });
+        $('.nice-border__vertical').each(function () {
+            $(this).empty();
+            height = $(this).css('height');
+            linesCount = Math.round(rm_px(height, true) / lineLen);
+            $(this).append(('<div class="nice-border__v-line"></div>').repeat(linesCount));
+        });
     })
-});
+}
